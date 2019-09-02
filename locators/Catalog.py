1fec6a41856bb6bab8db58ad412b269838404d25
Seva <vsiyavush91@gmail.com>
2019-09-02 16:16:08 +0300

    commit new tests in catalog - products

--- locators/Catalog.py
+++ locators/Catalog.py
@@ -2,8 +2,10 @@ class Catalog:
     save_button = "//button[@data-original-title='Save']"
     add_new_product = "//a[@data-original-title='Add New']"
     edit_product = "//a[@data-original-title='Edit']"
-    delete_button = "//a[data-original-title='Delete']"
-    # product_check_box =
+    delete_button = "//button[@data-original-title='Delete']"
+    filter_button = "//button[@id='button-filter']"
+    product_check_box = "//input[@name='selected[]']"
+    product_name_filter = "filter_name"
 
     main_menu = "#menu-catalog"
     products = "Products"
--- tests_admin/test_catalog_admin.py
+++ tests_admin/test_catalog_admin.py
@@ -1,8 +1,8 @@
 import time
 import pytest
-# import selenium.webdriver.common.alert.Alert
 
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.alert import Alert
 
 from locators import Login
 from locators import Catalog
@@ -19,13 +19,14 @@ def open_login_page(browser, request):
 
 
 @pytest.fixture(scope="function")
-def login(browser):
+def login(browser, request):
     """
         Логин в админке
     """
-    ins_user_name = browser.find_element(*Login.user_name).send_keys("admin")
-    ins_password = browser.find_element(*Login.password_field).send_keys("25191")
-    login_button = browser.find_element(*Login.login_button).click()
+    browser.find_element(*Login.user_name).send_keys("admin")
+    browser.find_element(*Login.password_field).send_keys("25191")
+    browser.find_element(*Login.login_button).click()
+
 
 
 @pytest.mark.usefixtures("open_login_page")
@@ -34,12 +35,12 @@ class TestCreateProducts:
     @pytest.mark.usefixtures("login")
     def test_open_catalog(self, browser):
         """
-            Атворизация в вдминке  Opencart
+        Тест открытия экрана авторизация
         """
 
         catalog_menu = browser.find_element_by_css_selector(Catalog.main_menu)
         catalog_menu.click()
-        product_menu = catalog_menu.find_element_by_link_text(Catalog.products).click()
+        catalog_menu.find_element_by_link_text(Catalog.products).click()
         time.sleep(2)
 
 
@@ -47,59 +48,37 @@ class TestCreateProducts:
         """
         Тест добавления нового продукта
         """
-        product_list = browser.find_element_by_xpath(Catalog.add_new_product).click()
-        input_product_name = browser.find_element_by_name(Catalog.product_name_field).send_keys("IphoneXR")
-        input_product_descript = browser.find_element_by_xpath(Catalog.product_description).send_keys("Some text")
-        input_product_meta = browser.find_element_by_name(Catalog.meta_tag_title).send_keys("Iphone XR")
-        open_data = browser.find_element_by_xpath(Catalog.data_tab).click()
-        input_model = browser.find_element_by_name(Catalog.model_field).send_keys("10R")
-        save_product = browser.find_element_by_xpath(Catalog.save_button).click()
+        browser.find_element_by_xpath(Catalog.add_new_product).click()
+        browser.find_element_by_name(Catalog.product_name_field).send_keys("IphoneXR")
+        browser.find_element_by_xpath(Catalog.product_description).send_keys("Some text")
+        browser.find_element_by_name(Catalog.meta_tag_title).send_keys("Iphone XR")
+        browser.find_element_by_xpath(Catalog.data_tab).click()
+        browser.find_element_by_name(Catalog.model_field).send_keys("10R")
+        browser.find_element_by_xpath(Catalog.save_button).click()
         assert browser.find_elements_by_xpath(Alerts.add_to_wish_alert)
         time.sleep(5)
-
-
-@pytest.mark.usefixtures("open_login_page")
-class TestEditProducts:
-
-    @pytest.mark.usefixtures("login")
-    def test_open_catalog(self, browser):
-        """
-            Атворизация в вдминке  Opencart
-        """
-
-        catalog_menu = browser.find_element_by_css_selector(Catalog.main_menu)
-        catalog_menu.click()
-        product_menu = catalog_menu.find_element_by_link_text(Catalog.products).click()
-        time.sleep(2)
 
 
     def test_edit(self, browser):
         """
             Тестирование редактирования первого продукта из списка
         """
-        open_edit = browser.find_element_by_xpath(Catalog.edit_product).click()
+        browser.find_element_by_xpath(Catalog.edit_product).click()
         nav_tab = browser.find_elements_by_css_selector(Catalog.product_nav_tabs)
         for item in nav_tab:
             ActionChains(browser).move_to_element(item).pause(1).perform()
-        save_product = browser.find_element_by_xpath(Catalog.save_button).click()
+        browser.find_element_by_xpath(Catalog.save_button).click()
         assert browser.find_elements_by_xpath(Alerts.add_to_wish_alert)
         time.sleep(5)
 
 
-# @pytest.mark.usefixtures("open_login_page")
-# class TestDeleteProducts:
-#
-#     @pytest.mark.usefixtures("login")
-#     def test_open_catalog(self, browser):
-#         """
-#             Атворизация в вдминке  Opencart
-#         """
-#         catalog_menu = browser.find_element_by_css_selector(Catalog.main_menu)
-#         catalog_menu.click()
-#         product_menu = catalog_menu.find_element_by_link_text(Catalog.products).click()
-#         time.sleep(2)
-#
-#
-#     # def test_delete(self,browser):
-
-
+    def test_delete(self, browser):
+         """
+            Тест удаления добавленного продукта
+         """
+         browser.find_element_by_name(Catalog.product_name_filter).send_keys("IphoneXR")
+         browser.find_element_by_xpath(Catalog.filter_button).click()
+         browser.find_element_by_xpath(Catalog.product_check_box).click()
+         browser.find_element_by_xpath(Catalog.delete_button).click()
+         Alert(browser).accept()
+         time.sleep(5)
